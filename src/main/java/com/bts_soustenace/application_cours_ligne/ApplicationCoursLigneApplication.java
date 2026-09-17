a35cad0bd99f223f31a53306b4be5423ecbb8b44
Joekisaki <joekisaki0@gmail.com>
2026-09-17 05:06:30 +0100

    Entyte du projet

---
+++ src/main/java/com/bts_soustenace/application_cours_ligne/ApplicationCoursLigneApplication.java
@@ -0,0 +1,13 @@
+package com.bts_soustenace.application_cours_ligne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationCoursLigneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationCoursLigneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bts_soustenace/application_cours_ligne/ApplicationCoursLigneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bts_soustenace.application_cours_ligne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationCoursLigneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
